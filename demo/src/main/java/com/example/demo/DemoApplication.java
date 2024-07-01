4f5bb79fb79638d7e33c220a06efde51455b6c6b
Sanjay-kumawat113 <146760417+Sanjay-kumawat113@users.noreply.github.com>
2024-07-01 08:17:13 +0530

    Update demo/src/main/java/com/example/demo/DemoApplication.java
    Co-authored-by: codiumai-pr-agent[bot] <138128286+codiumai-pr-agent[bot]@users.noreply.github.com>

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	
 	public static void main(String args[]){    
-String S11="java";//creating string by Java string literal    
+String javaString="java";//creating string by Java string literal
 char ch[]={'s','t','r','i','n','g','s'};    
 String s2=new String(ch);//converting char array to string    
 String s3=new String("example");//creating Java string by new keyword    
