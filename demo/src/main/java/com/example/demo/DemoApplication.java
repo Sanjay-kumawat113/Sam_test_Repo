44c8c561532818a12ac60d2dd02dd931deba4c2a
Sanjay-kumawat113 <146760417+Sanjay-kumawat113@users.noreply.github.com>
2024-07-01 08:13:39 +0530

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+
+
+
+
+
+	
 	public static void main(String args[]){    
-String s1="java";//creating string by Java string literal    
+String S11="java";//creating string by Java string literal    
 char ch[]={'s','t','r','i','n','g','s'};    
 String s2=new String(ch);//converting char array to string    
 String s3=new String("example");//creating Java string by new keyword    
-System.out.println(s1);    
+System.out.println(S11);    
 System.out.println(s2);    
 System.out.println(s3);    
 }}    
